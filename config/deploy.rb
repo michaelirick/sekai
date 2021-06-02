422f578e8b51c916872c8b115fbf0360062d7ffa
Miker Irick <michaelirick@gmail.com>
2021-06-02 17:43:08 -0500

    ask branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :pty, true
 # set :puma_bind, 'tcp://0.0.0.0:8181'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/yuusekai"
