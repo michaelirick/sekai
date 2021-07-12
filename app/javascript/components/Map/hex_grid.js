c2cccb6996743afc2967ce9c89903bf1de311ccb
Miker Irick <michaelirick@gmail.com>
2021-07-12 15:39:20 -0500

    abstracted hexoptions

--- app/javascript/components/Map/hex_grid.js
+++ app/javascript/components/Map/hex_grid.js
@@ -4,23 +4,18 @@ import {LayerGroup, Polygon, useMapEvents} from 'react-leaflet'
 import html from 'utils/html'
 import HexCell from './hex_cell'
 import api from 'utils/api'
-
-// width of 12 mi gives side 6.9282
-// 6.9282 / (25000/8192)
-const hexOptions = {
-  size: 6.9282 / (25000/8192),
-  strokeWidth: 1
-}
-
-const gridOptions  = {
-  width: 64,
-  height: 48
-};
-
-const HexFactory = extendHex(hexOptions);
-const Grid = defineGrid(HexFactory);
+import World from 'models/world'
 
 const HexGrid = (props) => {
+  const world = new World(props.world);
+  const gridOptions  = {
+    width: 64,
+    height: 48
+  };
+
+  const HexFactory = world.hexFactory();
+  const Grid = defineGrid(HexFactory);
+
   const [grid, setGrid] = useState(Grid()); // empty grid, make api call to populate from center and zoom
   const [center, setCenter] = useState(HexFactory().fromPoint(props.center)); // these are point cords
   const [zoom, setZoom] = useState(props.zoom);
@@ -77,7 +72,7 @@ const HexGrid = (props) => {
     return grid.map((h, i) => {
       return html.tag(HexCell, i, {
         hex: h,
-        options: hexOptions
+        options: world.hexOptions()
       });
     });
   }
